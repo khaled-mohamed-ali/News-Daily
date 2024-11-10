24f7de65d88e99e5662824b0e692f47c8df05600
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-11-10 23:22:59 +0200

    update today News fetching data mechanism

--- src/app/all-news/all-news.component.ts
+++ src/app/all-news/all-news.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { NewsDataService } from '../news-data.service';
 import { NewsData } from '../interface/news-data';
 
@@ -11,12 +11,22 @@ import { NewsData } from '../interface/news-data';
 })
 export class AllNewsComponent {
   private NewsDataService = inject(NewsDataService)
-  // newsData = signal<NewsData | undefined>(undefined) 
+  randomNews = this.NewsDataService.getRandomDayNews()
+  
+
 
   ngOnInit() {
     this.NewsDataService.getNews();
+    this.NewsDataService.getTodayNews();
+    this.NewsDataService.getRandomDayNews()
 
-    setTimeout(() => this.NewsDataService.getTodayNews(),6000)
+    
+  
+    setTimeout(() => console.log(this.randomNews?.title), 4000)
+
   }
+
+
+
 
 }
--- src/app/news-data.service.ts
+++ src/app/news-data.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { NewsData } from './interface/news-data';
 import { BrowserModule } from '@angular/platform-browser';
 import { map, filter } from 'rxjs/operators';
@@ -16,28 +16,30 @@ export class NewsDataService {
 
   private httpClient = inject(HttpClient);
   News = signal<NewsData | undefined>(undefined);
-  url = "https://newsapi.org/v2/everything?q=apple&from=2024-11-04&to=2024-11-04&sortBy=popularity&";
+  todayNews = this.getTodayNews();
+  url = "https://newsapi.org/v2/everything?q=apple&from=2024-11-04&to=2024-11-08&sortBy=popularity&";
   apiKey: string = "apiKey=d20e510c9ed54f6eaf96079fedec6bd2";
-
+  todayNewsLength: number| undefined = 0;
   
 
   getNews() {
     return (
       this.httpClient.get<NewsData>( this.url + this.apiKey)
         .subscribe({
-          next: (news: NewsData) => this.News.set(news)// Updates state with the fetched news
-          
+          next: (news: NewsData) => this.News.set(news)
         })
     )
   }
 
   getTodayNews() {
-     return  of(this.News()?.articles).pipe(
-      map(articles => articles?.filter(item => item.publishedAt.includes('2024-11-07')) )
-     ).subscribe({
-      next: (x) =>  console.log(x)
-     })
+    const todayNews = computed(() => this.News()?.articles.filter((item => item.publishedAt.includes('2024-11-06'))));
+    return todayNews
+  }
 
+  getRandomDayNews() {
+    const value = this.todayNews;
+    const randomIndex = Math.floor(Math.random() * value()!?.length);
+    return this.todayNews()?.[randomIndex]
   }
 
 
