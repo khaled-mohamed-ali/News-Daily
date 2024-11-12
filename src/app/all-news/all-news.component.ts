60757315a3b506a8471cf85594d5e353bf0aaa42
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-11-12 23:34:16 +0200

    change functionality to fully use to signals

--- src/app/all-news/all-news.component.ts
+++ src/app/all-news/all-news.component.ts
@@ -13,32 +13,28 @@ export class AllNewsComponent {
 
   private NewsDataService = inject(NewsDataService)
   News = signal<allNewsData | undefined>(undefined);
-  TodayNews = signal<News[] | undefined>(undefined);
-  randomTodayNews: News | undefined  ;  
+  
+  todayNews = computed(() => this.News()?.articles.filter((item => item.publishedAt.includes('2024-11-07'))));
 
+  RandomDayArticle = computed(() => {
+    const dayNews = this.todayNews();
+    const validArticles = dayNews?.filter(article => article.author != null);
+    const random = Math.floor(Math.random() * dayNews!?.length)
 
+    return validArticles?.[random]
 
+  })
+   
   ngOnInit() {
     this.NewsDataService.getNews().subscribe({
       next: (news: allNewsData) => {
         this.News.set(news);
-        this.getTodayNews();
-        this.getRandomDayNews()
-      }   
+
       }
-    )}
-  
-
-  getTodayNews() {
-    const todayNews = computed(() => this.News()?.articles.filter((item => item.publishedAt.includes('2024-11-06'))));
-    this.TodayNews.set(todayNews());
+    }
+    )
   }
 
-  getRandomDayNews() {
-    const newsItem = this.TodayNews();
-    const randomIndex = Math.floor(Math.random() * newsItem!?.length);
-    console.log(randomIndex,'random index');
-    newsItem?.[randomIndex].author === null || undefined ? this.getRandomDayNews() : this.randomTodayNews = newsItem?.[randomIndex];
-  }
+
 }
 
