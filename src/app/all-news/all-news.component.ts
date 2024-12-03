ef740a76bdf16950809421aa1e822a8f9c06cf56
unknown <khaled.mohamed.ali@gmail.com>
2024-12-03 21:08:12 +0200

    update mechanism of get 5 small articles randomly

--- src/app/all-news/all-news.component.ts
+++ src/app/all-news/all-news.component.ts
@@ -14,51 +14,6 @@ export class AllNewsComponent {
 
   private NewsDataService = inject(NewsDataService)
 
-  defaultNews: AllNewsData = {
-    articles: []
-  };
-
-  News = signal<AllNewsData>(this.defaultNews);
-  todayNews = computed(() => this.News()?.articles.filter((item => item.publishedAt.includes('2024-11-07'))));
-
-
-
-  RandomDayArticle = computed(() => {
-
-    const dayNews = this.todayNews();
-    const validArticles = dayNews?.filter(article =>  article.urlToImage);
-    const random = Math.floor(Math.random() * validArticles!?.length);
-
-    return validArticles?.[random];
-
-  })
-
-  rightSideArticle: News | undefined = undefined
-
-  get4News(): News [] {   
-    const dayNews = this.todayNews();
-    const validArticles = dayNews?.filter(article =>  article.urlToImage);
-
-      const items: News []  = [] ;
-      
-      const fetchArticles = () => {
-      const random = Math.floor(Math.random() * validArticles!?.length)
-
-      if (items.length >= 5) {
-        this.rightSideArticle =  items.pop();
-        return;
-      }
-
-      items.push(validArticles[random]);
-        fetchArticles();
-    };
-      fetchArticles();
-
-    return items;
-  }
-
-
-
   ngOnInit() {
     this.NewsDataService.getNews().subscribe({
       next: (news: AllNewsData) => {
@@ -69,13 +24,65 @@ export class AllNewsComponent {
       complete: () => this.get4News(),
     }
     )
-    setTimeout(()=> console.log(this.rightSideArticle,'work') ,3000)
+  }
+
+  defaultNews: AllNewsData = {
+    articles: []
+  };
+
+
+
+  News = signal<AllNewsData>(this.defaultNews);
+  todayNews = computed(() => this.News()?.articles.filter((item => item.publishedAt.includes('2024-11-07'))));
+
+
+
+  RandomDayArticle = computed(() => {
+
+    const dayNews = this.todayNews();
+    const validArticles = dayNews?.filter(article => article.urlToImage);
+    const random = Math.floor(Math.random() * validArticles!?.length);
+
+    return validArticles?.[random];
+
+  })
+
+  rightSideArticle: News | undefined = undefined
+
+  get4News(): News[] {
+    const dayNews = this.todayNews();
+    const validArticles = dayNews?.filter(article => article.urlToImage);
+    const random = Math.floor(Math.random() * (validArticles!?.length -5) )
+    const items: News[] = [];
+
+    for (let i = random; validArticles.length && i < random + 5; i++) {
+      items.push(validArticles[i])
+    }
+
+    this.rightSideArticle = items.pop();
+
+
+    //   const fetchArticles = () => {
+    //   const random = Math.floor(Math.random() * validArticles!?.length)
+
+    //   if (items.length >= 5) {
+    //     this.rightSideArticle =  items.pop();
+    //     return;
+    //   }
+
+    //   items.push(validArticles[random]);
+    //     fetchArticles();
+    // };
+    //   fetchArticles();
+
+    return items;
   }
 
 
+
+
+
+
 }
 
-function ngOnInit() {
-  throw new Error('Function not implemented.');
-}
 
