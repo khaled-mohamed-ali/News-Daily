df8ea2d881aab0e0490cd636befe688855006cd7
unknown <khaled.mohamed.ali@gmail.com>
2024-12-04 17:28:28 +0200

    project user info

--- src/app/all-news/small-news-card/small-news-card.component.ts
+++ src/app/all-news/small-news-card/small-news-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { News } from '../../interface/news-data';
+import { PostInfoComponent } from "../post-info/post-info.component";
 
 @Component({
   selector: 'app-small-news-card',
   standalone: true,
-  imports: [],
+  imports: [PostInfoComponent],
   templateUrl: './small-news-card.component.html',
   styleUrl: './small-news-card.component.css'
 })
