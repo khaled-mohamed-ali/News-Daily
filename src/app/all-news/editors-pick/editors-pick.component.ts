0fd54d88c8f4bf74e5f7ff283ad62c4b4747e498
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-12-18 13:45:54 +0200

    update edtors-pick fullsize card

--- src/app/all-news/editors-pick/editors-pick.component.ts
+++ src/app/all-news/editors-pick/editors-pick.component.ts
@@ -3,11 +3,12 @@ import { SmallNewsCardComponent } from "../small-news-card/small-news-card.compo
 import { AllNewsData, News } from '../../interface/news-data';
 import { MidSizeCardComponent } from "../mid-size-card-img/mid-size-card.component";
 import { PostInfoComponent } from "../post-info/post-info.component";
+import { FullSizeCardComponent } from "../full-size-card/full-size-card.component";
 
 @Component({
   selector: 'app-editors-pick',
   standalone: true,
-  imports: [SmallNewsCardComponent, MidSizeCardComponent, PostInfoComponent],
+  imports: [SmallNewsCardComponent, MidSizeCardComponent, PostInfoComponent, FullSizeCardComponent],
   templateUrl: './editors-pick.component.html',
   styleUrl: './editors-pick.component.css'
 })
