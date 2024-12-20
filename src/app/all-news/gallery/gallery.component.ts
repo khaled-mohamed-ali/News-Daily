2d6b29a87cec61a25483992da93868fc255301b7
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-12-20 20:54:09 +0200

    update data in gallery component

--- src/app/all-news/gallery/gallery.component.ts
+++ src/app/all-news/gallery/gallery.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { News } from '../../interface/news-data';
+import { MidSizeCardComponent } from "../mid-size-card-img/mid-size-card.component";
+import { SmallNewsCardComponent } from "../small-news-card/small-news-card.component";
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [MidSizeCardComponent, SmallNewsCardComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GALLERYComponent {
+
+    generalNews = input<News[] | undefined>()
+  
 
 }
