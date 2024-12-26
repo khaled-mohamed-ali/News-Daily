34244d489ff98a758db1e91cae8a7f0da87a7629
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-12-26 14:19:42 +0200

    update story component last part data

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { News } from '../interface/news-data';
+import { SmallNewsCardComponent } from "../all-news/small-news-card/small-news-card.component";
 
 @Component({
   selector: 'app-story',
   standalone: true,
-  imports: [],
+  imports: [SmallNewsCardComponent],
   templateUrl: './story.component.html',
   styleUrl: './story.component.css'
 })
 export class StoryComponent {
+
+    News = input<News[] | undefined>()
+  
 
 }
