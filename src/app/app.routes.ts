785bf654e3c6d58e5c3e3ef18242601a969929d6
unknown <khaled.mohamed.ali@gmail.com>
2025-02-05 17:48:00 +0200

    adding routing ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavigateComponent } from './navigate/navigate.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '/Navigate', component: NavigateComponent },
+
+];
