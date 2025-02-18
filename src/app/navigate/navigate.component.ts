b6f3d0bf2ab82c9756c669c847c4eafe5100c964
unknown <khaled.mohamed.ali@gmail.com>
2025-02-18 18:11:45 +0200

    update navigate 2

--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { NewsDataService } from '../news-data.service';
 import { AllNewsComponent } from '../all-news/all-news/all-news.component';
+import { AllNewsData } from '../interface/news-data';
 
 @Component({
   selector: 'app-navigate',
@@ -9,17 +10,17 @@ import { AllNewsComponent } from '../all-news/all-news/all-news.component';
   templateUrl: './navigate.component.html',
   styleUrl: './navigate.component.css'
 })
+
 export class NavigateComponent {
-    private NewsDataService = inject(NewsDataService)
-  
-  category : string = 'business'
+  private NewsDataService = inject(NewsDataService)
+
+  catigoryNews = signal<AllNewsData | null>(null);
+  category: string = 'business';
+
   ngOnInit() {
     this.NewsDataService.getNewsByCatigory(this.category).subscribe({
-      // next: (news:AllNewsComponent)=> {
-        
-      // }
+      next: (News) => this.catigoryNews.set(News)
     })
-    console.log('rendred')
   }
 
 }
