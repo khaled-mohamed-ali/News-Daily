a36a54df6d8ed7ac0e03f4feb0e4a594a2667614
unknown <khaled.mohamed.ali@gmail.com>
2025-03-09 12:03:16 +0200

    update the pagination mechanism

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -12,51 +12,69 @@ import { NewsDataService } from '../news-data.service';
 export class PaginationComponent {
   NewsDataService = inject(NewsDataService);
   router = inject(Router)
-  pagenumber = signal(1);
+  currentPageNumber = signal<number>(1);
   @Input() navSelection!: string;
   pagesNumber = computed(() => (Math.floor(this.NewsDataService.categoryNews()?.['totalResults'] / 20)))
+  pagination = signal<number>(1);
+
 
   constructor() {
     effect(() => {
-      console.log(this.pagesNumber(),'in effec')
+      console.log(this.pageNumbers, 'in effec')
     });
   }
 
   get pageNumbers() {
-    return Array.from({ length: this.pagesNumber()}, (i:number) => i++);
+    return Array.from({ length: this.pagesNumber() }, (_, i) => i).slice(this.pagination(), this.pagination() + 4);
   }
 
 
   nextPage() {
-    this.pagenumber.update(prev => prev + 1);
-    this.NewsDataService.getNewsByCatigory(this.navSelection,this.pagenumber());
-    this.router.navigate(['/', this.navSelection,this.pagenumber()]);
+    this.currentPageNumber.update(prev => prev + 1);
+    this.NewsDataService.getNewsByCatigory(this.navSelection, this.currentPageNumber());
+    this.router.navigate(['/', this.navSelection, this.currentPageNumber()]);
+    this.pagination() + 4 - this.currentPageNumber() == 1 ? this.pagination.update(prev => prev + 1) : ''
   }
 
 
 
   prevPage() {
-    this.pagenumber.update(prev => prev- 1);
-    this.NewsDataService.getNewsByCatigory(this.navSelection,this.pagenumber());
-    this.router.navigate( ['/', this.navSelection, this.pagenumber()]);
+    this.currentPageNumber.update(prev => prev - 1);
+    this.NewsDataService.getNewsByCatigory(this.navSelection, this.currentPageNumber());
+    this.router.navigate(['/', this.navSelection, this.currentPageNumber()]);
+    this.pagination() - this.currentPageNumber() == 1 ? this.pagination.update(prev => prev - 1) : ''
 
   }
 
   numberedPage(pageN: number) {
-    this.pagenumber.set(pageN);
-    this.NewsDataService.getNewsByCatigory(this.navSelection, this.pagenumber());
-    this.router.navigate(['/', this.navSelection, this.pagenumber()]);
+    this.currentPageNumber.set(pageN);
+    this.NewsDataService.getNewsByCatigory(this.navSelection, this.currentPageNumber());
+
+    const paginationValue = this.pagination();
+    const currentPage = this.currentPageNumber();
+    console.log(paginationValue,currentPage)
+    
+    if (paginationValue + 4 - currentPage === 1) {
+      this.pagination.update(prev => prev + 1);
+    } else if (paginationValue - currentPage === 0) {
+      this.pagination.update(prev => prev - 1);
+    }
+    
+    else {
+      this.pagination.set(1)
+    }
+    this.router.navigate(['/', this.navSelection, this.currentPageNumber()]);
   }
 
   ngOnInit() {
     console.log(this.pageNumbers, 'in ng')
   }
 
-ngOnChanges(changes: SimpleChange) {
-  if(changes) {
-    this.pagenumber.set(1)
+  ngOnChanges(changes: SimpleChange) {
+    if (changes) {
+      this.currentPageNumber.set(1)
+    }
   }
-}
 
 
 }
