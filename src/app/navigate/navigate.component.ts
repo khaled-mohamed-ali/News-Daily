0405c3acd9d2e5f47f290633c92d886f1258ea40
unknown <khaled.mohamed.ali@gmail.com>
2025-03-10 11:15:22 +0200

    fix functional bug in news-aricle nav

--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -23,7 +23,7 @@ export class NavigateComponent {
   newsByCategory = signal<AllNewsData | undefined>(undefined);
   
   getNewsArticle(news: any) {
-    return this.router.navigateByUrl('newsaritcle', { state: { news: news }});
+    return this.router.navigateByUrl('/newsaritcle', { state: { article: news }});
   }
 
   ngDoCheck() {
